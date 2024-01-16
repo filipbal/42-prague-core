99c55c1d0ef7b7ab08233d54f597caa412a301da
filipbal <filip.balakovski@gmail.com>
2024-01-16 13:03:56 +0100

    norme fix

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -28,8 +28,8 @@ int main(void)
 */
 int	ft_isdigit(int c)
 {
-    if (c >= 48 && c <= 57)
-        return (1);
-    else
-        return (0);
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
 }

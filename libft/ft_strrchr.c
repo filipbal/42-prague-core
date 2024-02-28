576f56fbdee365aa93ae9662e0b96b3ffcf1f1f8
Filip Balakovski <filip.balakovski@gmail.com>
2024-02-28 23:44:57 +0100

    added ft_strrchr

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                        :+:      :+:    :+:  */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbalakov <fbalakov@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 11:08:01 by fbalakov          #+#    #+#             */
+/*   Updated: 2024/02/27 11:08:01 by fbalakov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+#include <stdio.h>
+char *ft_strrchr(const char *s, int c);
+int	main(void)
+{
+	const char s[] = "Filip";
+	for (int i = 0; s[i] >= '\0'; i++)
+	{
+		printf("%p: %c\n", (void *)&s[i], s[i]);
+	}
+	int c = 'i';
+	char *p = (void *)ft_strrchr(s, c);
+	if (p == NULL)
+		printf("\nCharacter not found.\n");
+	else
+		printf("\nCharacter '%c' is at address: %p\n", c, p);
+	return (0);
+}
+*/
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	i++;
+	while (i > 0)
+	{
+		if (s[i] == c)
+			return ((char *)&s[i]);
+		i--;
+	}
+	return (NULL);
+}

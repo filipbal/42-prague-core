ceb04bcac6c14f7ccdd71eade8a194047fae0184
Filip Balakovski <fbalakov@c2r1s5.42prague.com>
2024-03-17 15:47:23 +0100

    added ft_strmapi LIBFT

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 15:46:27 by fbalakov          #+#    #+#             */
+/*   Updated: 2024/03/17 15:46:59 by fbalakov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// The purpose of ft_strmapi is to apply the given function f to each character
+// in the input string s and create a new string based on the results.
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}

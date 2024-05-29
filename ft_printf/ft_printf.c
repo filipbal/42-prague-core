9256ae87fb216c2320e6dccc8ca155deae97ecab
Filip Balakovski <fbalakov@c2r2s5.42prague.com>
2024-05-29 19:00:28 +0200

    implemented basic main function

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,8 +6,31 @@
 /*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 10:38:19 by fbalakov          #+#    #+#             */
-/*   Updated: 2024/05/29 17:41:09 by fbalakov         ###   ########.fr       */
+/*   Updated: 2024/05/29 18:15:34 by fbalakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+// returns a number of % characters in a string
 int ft_printf(const char *, ...);
+
+int main (void)
+{
+	char 	str[100] = "My name is %s and I am %d years old.";
+	int		count;
+	int		i;
+	
+	count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+			count++;
+		i++;
+	}
+	count += 48;
+	write(1, &count , 1);
+	write(1, "\n" , 1);
+	return (0);
+}

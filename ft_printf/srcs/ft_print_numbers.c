05dfd1d155d78767cc96f3abe7bfaef00bd216d8
Filip Balakovski <fbalakov@c2r1s5.42prague.com>
2024-10-24 11:42:56 +0200

    Implement integer related functions

--- ft_printf/srcs/ft_print_numbers.c
+++ ft_printf/srcs/ft_print_numbers.c
@@ -6,7 +6,73 @@
 /*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 13:11:37 by fbalakov          #+#    #+#             */
-/*   Updated: 2024/10/22 13:11:38 by fbalakov         ###   ########.fr       */
+/*   Updated: 2024/10/24 11:41:42 by fbalakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 11:15:34 by fbalakov          #+#    #+#             */
+/*   Updated: 2024/10/24 11:35:31 by fbalakov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf.h"
+
+static int	ft_count_digits(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len = 1;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+static int	ft_count_uint_digits(unsigned int n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+static void	ft_put_uint_fd(unsigned int n, int fd)
+{
+	if (n >= 10)
+	{
+		ft_put_uint_fd(n / 10, fd);
+		ft_put_uint_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
+}
+
+int	ft_print_int(int n)
+{
+	ft_putnbr_fd(n, 1);
+	return (ft_count_digits(n));
+}
+
+int	ft_print_uint(unsigned int n)
+{
+	ft_put_uint_fd(n, 1);
+	return (ft_count_uint_digits(n));
+}

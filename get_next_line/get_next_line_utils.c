112d723775f41578a9d13436ab16bef9185277ac
Filip Balakovski <fbalakov@c2r1s5.42prague.com>
2024-10-25 14:43:17 +0200

    Add GNL

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 12:39:29 by fbalakov          #+#    #+#             */
+/*   Updated: 2024/10/25 13:41:08 by fbalakov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen_gnl(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr_gnl(const char *s, int c)
+{
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+static char	*join_and_free(char *s1, char *s2, char *str)
+{
+	size_t	i;
+	size_t	j;
+
+	i = -1;
+	while (s1[++i])
+		str[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		str[i + j] = s2[j];
+	str[i + j] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*ft_strjoin_gnl(char *s1, char *s2)
+{
+	char	*str;
+
+	if (!s1)
+	{
+		s1 = malloc(1);
+		if (!s1)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	if (!s2)
+	{
+		free(s1);
+		return (NULL);
+	}
+	str = malloc(ft_strlen_gnl(s1) + ft_strlen_gnl(s2) + 1);
+	if (!str)
+	{
+		free(s1);
+		return (NULL);
+	}
+	return (join_and_free(s1, s2, str));
+}
+
+char	*read_and_append(int fd, char *stash, char *buffer)
+{
+	int	bytes_read;
+
+	bytes_read = 1;
+	while (bytes_read > 0 && !ft_strchr_gnl(stash, '\n'))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (NULL);
+		buffer[bytes_read] = '\0';
+		stash = ft_strjoin_gnl(stash, buffer);
+		if (!stash)
+			return (NULL);
+	}
+	return (stash);
+}

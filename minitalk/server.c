1e97b368228a39f455c1b93eef6c114abeb40ead
Filip Balakovski <filip.balakovski@gmail.com>
2024-12-12 12:51:03 +0100

    Fix The Norm in server.c

--- minitalk/server.c
+++ minitalk/server.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/12 10:38:19 by fbalakov          #+#    #+#             */
-/*   Updated: 2024/12/12 09:56:16 by fbalakov         ###   ########.fr       */
+/*   Created: 2024/12/12 12:47:02 by fbalakov          #+#    #+#             */
+/*   Updated: 2024/12/12 12:47:02 by fbalakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,20 +27,16 @@ static int	g_bit_position = 0;
  */
 void	handle_signal(int signum, siginfo_t *info, void *context)
 {
-	(void)context;  // Unused parameter
-
+	(void)context;
 	if (signum == SIGUSR2)
 		g_current_char |= (1 << g_bit_position);
 	g_bit_position++;
-
-	// When we've received all 8 bits of a character
 	if (g_bit_position == 8)
 	{
 		ft_putchar_fd(g_current_char, 1);
 		g_current_char = 0;
 		g_bit_position = 0;
 	}
-	// Send acknowledgment signal back to client
 	kill(info->si_pid, SIGUSR1);
 }
 
@@ -54,27 +50,25 @@ void	setup_signal_handlers(void)
 	struct sigaction	sa;
 
 	sa.sa_sigaction = handle_signal;
-	sa.sa_flags = SA_SIGINFO;  // Enable extended signal handling
+	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
-
-	// Register handlers for both SIGUSR1 and SIGUSR2
-	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+	if (sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
 	{
 		ft_printf("Error: Could not set up signal handlers\n");
 		exit(1);
 	}
 }
 
+// Print server's PID so clients can connect
+// Set up signal handlers
+// Keep the server running indefinitely
+// Pause until a signal is received
 int	main(void)
 {
-	// Print server's PID so clients can connect
 	ft_printf("Server PID: %d\n", getpid());
-	
-	// Set up signal handlers
 	setup_signal_handlers();
-
-	// Keep the server running indefinitely
 	while (1)
-		pause();  // Pause until a signal is received
+		pause();
 	return (0);
 }

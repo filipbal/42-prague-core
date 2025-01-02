9b91fc75517f7158d19cdddced420c875da228cd
Filip Balakovski <filip.balakovski@gmail.com>
2025-01-02 13:03:37 +0100

    Redefine linux key codes and event handlers

--- so_long/includes/so_long.h
+++ so_long/includes/so_long.h
@@ -22,16 +22,16 @@
 # include <fcntl.h>        /* For open() */
 # include <unistd.h>       /* For close() */
 
-/* Define key codes for macOS */
-# define KEY_ESC     53
-# define KEY_W       13
-# define KEY_A       0
-# define KEY_S       1
-# define KEY_D       2
-# define KEY_UP      126
-# define KEY_LEFT    123
-# define KEY_DOWN    125
-# define KEY_RIGHT   124
+/* Define key codes for Linux/WSL */
+# define KEY_ESC     65307
+# define KEY_W       119
+# define KEY_A       97
+# define KEY_S       115
+# define KEY_D       100
+# define KEY_UP      65362
+# define KEY_LEFT    65361
+# define KEY_DOWN    65364
+# define KEY_RIGHT   65363
 
 /* Define window properties */
 # define TILE_SIZE   32

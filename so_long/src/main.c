d09145919f247aeeabc8f76216c337c30d54719f
Filip Balakovski <fbalakov@c2r1s5.42prague.com>
2025-01-10 10:00:43 +0100

    Simplify main, all errors to stderr, Norm OK

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -6,7 +6,7 @@
 /*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 18:00:11 by fbalakov          #+#    #+#             */
-/*   Updated: 2025/01/01 18:00:11 by fbalakov         ###   ########.fr       */
+/*   Updated: 2025/01/10 09:59:20 by fbalakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,51 +25,25 @@ static int	check_file_extension(char *filename)
 	return (!ft_strncmp(filename + len - 4, ".ber", 4));
 }
 
-/* 
-** Initialize all game variables to 0/NULL
-** Parse and validate the map
-** Initialize the game engine and window
-** Set up event handlers
-** Display initial game state
-** Render initial game state
-** Start the game loop 
-*/
-static int	start_game(char *map_file)
+/* Initialize game structure, validate inputs, parse map, initialize graphics,
+** and start game loop. All errors exit with error message to stderr. */
+int	main(int argc, char **argv)
 {
 	t_game	game;
 
 	ft_memset(&game, 0, sizeof(t_game));
-	if (!parse_map(&game, map_file))
-	{
-		ft_printf("Error\nInvalid map configuration\n");
-		cleanup_game(&game);
-		return (0);
-	}
+	if (argc != 2)
+		error_exit("Usage: ./so_long <map_file.ber>", &game);
+	if (!check_file_extension(argv[1]))
+		error_exit("Invalid file extension (must be .ber)", &game);
+	if (!parse_map(&game, argv[1]))
+		error_exit("Invalid map configuration", &game);
 	if (!init_game(&game))
-	{
-		ft_printf("Error\nFailed to initialize game\n");
-		cleanup_game(&game);
-		return (0);
-	}
+		error_exit("Failed to initialize game", &game);
 	ft_printf("Welcome to Betty's Adventure!\n");
 	ft_printf("Collect all %d bones and reach the bed!\n", game.collectibles);
 	ft_printf("Use WASD or arrow keys to move\n");
 	render_game(&game);
 	mlx_loop(game.mlx);
-	return (1);
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc != 2)
-	{
-		ft_printf("Error\nUsage: ./so_long <map_file.ber>\n");
-		return (1);
-	}
-	if (!check_file_extension(argv[1]))
-	{
-		ft_printf("Error\nInvalid file extension (must be .ber)\n");
-		return (1);
-	}
-	return (!start_game(argv[1]));
+	return (0);
 }

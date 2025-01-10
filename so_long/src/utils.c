2e2276168bd6d9d8ac329df7823a5da9a32d8f0b
Filip Balakovski <fbalakov@c2r1s5.42prague.com>
2025-01-10 12:24:44 +0100

    Remove count_lines and map_copy redundant utils

--- so_long/src/utils.c
+++ so_long/src/utils.c
@@ -6,61 +6,11 @@
 /*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 18:02:22 by fbalakov          #+#    #+#             */
-/*   Updated: 2025/01/09 12:24:14 by fbalakov         ###   ########.fr       */
+/*   Updated: 2025/01/10 12:23:44 by fbalakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-/* Count lines in a file
-** Handle EOF or error
-** Free the line after counting */
-int	count_lines(char *filename)
-{
-	int		fd;
-	int		lines;
-	char	*line;
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (-1);
-	lines = 0;
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
-		lines++;
-		free(line);
-	}
-	close(fd);
-	return (lines);
-}
-
-/* Create a copy of the map for validation purposes */
-char	**map_copy(char **map, int height)
-{
-	char	**copy;
-	int		i;
-
-	copy = (char **)malloc(sizeof(char *) * height);
-	if (!copy)
-		return (NULL);
-	i = 0;
-	while (i < height)
-	{
-		copy[i] = ft_strdup(map[i]);
-		if (!copy[i])
-		{
-			while (--i >= 0)
-				free(copy[i]);
-			free(copy);
-			return (NULL);
-		}
-		i++;
-	}
-	return (copy);
-}
 
 /* Error handling function */
 void	error_exit(char *message, t_game *game)

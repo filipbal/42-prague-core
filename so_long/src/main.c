0e9f492997fde0792ab14847a03e5e7fda6464ee
Filip Balakovski <fbalakov@c2r1s5.42prague.com>
2025-01-10 12:37:42 +0100

    Add err handling to main

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -6,11 +6,21 @@
 /*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 18:00:11 by fbalakov          #+#    #+#             */
-/*   Updated: 2025/01/10 10:49:26 by fbalakov         ###   ########.fr       */
+/*   Updated: 2025/01/10 12:26:24 by fbalakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+/* Error handling function */
+void	error_exit(char *message, t_game *game)
+{
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd(message, 2);
+	ft_putstr_fd("\n", 2);
+	cleanup_game(game);
+	exit(1);
+}
 
 /* Validate file extension (.ber) */
 static int	check_file_extension(char *filename)
